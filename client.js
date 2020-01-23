ab6c9769bcbe3cd5e96bab74256ffde97b3cbdc4
daniel5908 <danth343@student.liu.se>
2020-01-23 15:58:29 +0100

    Added log in functionality

--- client.js
+++ client.js
@@ -1,58 +1,73 @@
 function validateLogin(event) {
-  event.preventDefault();
-  let fields = event.target.elements;
-  const minLenPass = 4;
-  console.log(fields);
-  if (fields.password.value.length <= minLenPass) {
-    const _msg = `Passwords is to short need to be at least ${minLenPass}`;
-    console.warn(_msg);
-    document.getElementById("LV-Login-Form-Message").innerText = `${_msg} \n`;
-    return;
-  }
+    event.preventDefault();
+    let fields = event.target.elements;
+    const minLenPass = 4;
+    console.log(fields);
+    if (fields.password.value.length <= minLenPass) {
+	const _msg = `Passwords is to short need to be at least ${minLenPass}`;
+	console.warn(_msg);
+	document.getElementById("LV-Login-Form-Message").innerText = `${_msg} \n`;
+	return;
+    }
+    const token = serverstub.signIn(fields.username.value, fields.password.value);
+    if (token.success === true && 'data' in token) {
+	window.sessionStorage.setItem("token", token.data);
+	const _msg = `Successfully logged in`;
+	document.getElementById("LV-Login-Form-Message").innerText = `${_msg} \n`;
+    }
+    else {
+	const _msg = `Failed to log in`;
+	console.warn(_msg);
+	document.getElementById("LV-Login-Form-Message").innerText = `${_msg} \n`;	
+    }
+    
+	
+
+    
 }
 
 function validateSignUp(event) {
-  event.preventDefault();
-  let fields = event.target.elements;
-  const minLenPass = 1;
-  let errorElem = document.getElementById("LV-SignUp-Form-Message");
+    event.preventDefault();
+    let fields = event.target.elements;
+    const minLenPass = 1;
+    let errorElem = document.getElementById("LV-SignUp-Form-Message");
 
-  if (fields.password.value.length < minLenPass) {
-    const _msg = `Passwords is to short need to be at least ${minLenPass}`;
-    console.warn(_msg);
-    errorElem.innerText = `${_msg} \n`;
-    return;
-  }
+    if (fields.password.value.length < minLenPass) {
+	const _msg = `Passwords is to short need to be at least ${minLenPass}`;
+	console.warn(_msg);
+	errorElem.innerText = `${_msg} \n`;
+	return;
+    }
 
-  if (fields.password.value !== fields.password2.value) {
-    const _msg = "Passwords do not match";
-    console.warn(_msg);
-    errorElem.innerText = `${_msg} \n`;
-    return;
-  }
-  const postMsg = {
-    email: fields.email.value,
-    password: fields.password.value,
-    firstname: fields.firstname.value,
-    familyname: fields.familyname.value,
-    gender: fields.gender.value,
-    city: fields.city.value,
-    country: fields.country.value
-  };
+    if (fields.password.value !== fields.password2.value) {
+	const _msg = "Passwords do not match";
+	console.warn(_msg);
+	errorElem.innerText = `${_msg} \n`;
+	return;
+    }
+    const postMsg = {
+	email: fields.email.value,
+	password: fields.password.value,
+	firstname: fields.firstname.value,
+	familyname: fields.familyname.value,
+	gender: fields.gender.value,
+	city: fields.city.value,
+	country: fields.country.value
+    };
 
-  errorElem.innerText = serverstub.signUp(postMsg).message;
+    errorElem.innerText = serverstub.signUp(postMsg).message;
 }
 
 displayView = function() {
-  // the code required to display a view
+    // the code required to display a view
 };
 window.onload = function() {
-  //code that is executed as the page is loaded.
-  //You shall put your own custom code here.
-  //window.alert() is not allowed to be used in your implementation.
-  //window.alert("Hello TDDD97!");
-  let view = this.document.getElementById("load-welcome-view").innerHTML;
-  let mainView = this.document.getElementById("main-view");
-  mainView.innerHTML = view;
-  //   this.document.getElementById("SignUpForm").onsubmit() = this.validateSignUp;
+    //code that is executed as the page is loaded.
+    //You shall put your own custom code here.
+    //window.alert() is not allowed to be used in your implementation.
+    //window.alert("Hello TDDD97!");
+    let view = this.document.getElementById("load-welcome-view").innerHTML;
+    let mainView = this.document.getElementById("main-view");
+    mainView.innerHTML = view;
+    //   this.document.getElementById("SignUpForm").onsubmit() = this.validateSignUp;
 };
