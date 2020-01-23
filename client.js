48571b2af3ebff5618c6c3293698b7f7bdc8364b
Bjorn <bjorn@vyer.com>
2020-01-23 14:57:41 +0100

    Add singup form onsubmit validation

--- client.js
+++ client.js
@@ -1,3 +1,23 @@
+function validateSignUp(event) {
+  event.preventDefault();
+  let fields = event.target.elements;
+  let msg = "";
+
+  if (fields.Password.value.length <= minLenPass) {
+    const _msg = `Passwords is to short need to be at least ${minLenPass}`;
+    console.warn(_msg);
+    document.getElementById("LV-Form-Message").innerText = `${_msg} \n`;
+    return;
+  }
+
+  if (fields.Password.value !== fields.Password2.value) {
+    const _msg = "Passwords do not match";
+    console.warn(_msg);
+    document.getElementById("LV-Form-Message").innerText = _`${_msg} \n`;
+    return;
+  }
+}
+
 displayView = function() {
   // the code required to display a view
 };
@@ -9,4 +29,5 @@ window.onload = function() {
   let view = this.document.getElementById("load-welcome-view").innerHTML;
   let mainView = this.document.getElementById("main-view");
   mainView.innerHTML = view;
+  //   this.document.getElementById("SignUpForm").onsubmit() = this.validateSignUp;
 };
