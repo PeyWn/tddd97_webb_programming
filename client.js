e4d34ccca607e9bc1420ee0c928d6e8d8925a763
daniel5908 <danth343@student.liu.se>
2020-01-23 12:19:38 +0100

    Fixed initial frontend view

---
+++ client.js
@@ -0,0 +1,9 @@
+displayView = function(){
+    // the code required to display a view
+};
+window.onload = function(){
+    //code that is executed as the page is loaded.
+    //You shall put your own custom code here.
+    //window.alert() is not allowed to be used in your implementation.
+    //window.alert("Hello TDDD97!");
+};
