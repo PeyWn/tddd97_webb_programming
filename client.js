7e1065bad2190784a33a15228f8f29bb84cb6133
Bjorn <bjorn@vyer.com>
2020-01-23 12:48:35 +0100

    On page load set mainView innerHTML to welcome script

--- client.js
+++ client.js
@@ -1,9 +1,12 @@
-displayView = function(){
-    // the code required to display a view
+displayView = function() {
+  // the code required to display a view
 };
-window.onload = function(){
-    //code that is executed as the page is loaded.
-    //You shall put your own custom code here.
-    //window.alert() is not allowed to be used in your implementation.
-    //window.alert("Hello TDDD97!");
+window.onload = function() {
+  //code that is executed as the page is loaded.
+  //You shall put your own custom code here.
+  //window.alert() is not allowed to be used in your implementation.
+  //window.alert("Hello TDDD97!");
+  let view = this.document.getElementById("load-welcome-view").innerHTML;
+  let mainView = this.document.getElementById("main-view");
+  mainView.innerHTML = view;
 };
