05a7a377839875d2e8f67f56243d255ecdaa829c
Bjorn <bjorn.hvass@gmail.com>
2020-02-25 16:45:37 +0100

    Draft Project hasing

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -2,6 +2,8 @@
 from random import randrange
 import json
 import database_handler
+from session import new_session
+
 from flask import Flask, request
 from flask_bcrypt import Bcrypt
 
@@ -11,71 +13,10 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-class Session:
-    def __init__(self):
-        self.__logged_in_users = {}
-
-    def get_email_by_token(self, token):
-        for email, val in self.__logged_in_users.items():
-            if val['token'] == token:
-                return email
-        return None
-
-    def get_token_by_email(self, email):
-        return self.__logged_in_users[email]['token']
-
-    def get_connection_by_token(self, token):
-        email = self.get_email_by_token(token)
-        return self.__logged_in_users[email]['socket']
-
-    def generate_token(self):
-        letters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-        token = ""
-        for _ in range(0, 35):
-            token += letters[randrange(1, len(letters))]
-        if token in self.__logged_in_users:
-            return self.generate_token()
-        return token
-
-    def has_valid_session(self, email):
-        if email in self.__logged_in_users.keys():
-            token = self.get_token_by_email(email)
-            if token != None and self.has_valid_token(token):
-                return True
-        return False
-
-    def has_valid_token(self, token):
-        for item in self.__logged_in_users.values():
-            if token == item['token']:
-                return True
-        return False
-
-    def add_connection(self, token, socket):
-        email = self.get_email_by_token(token)
-        self.__logged_in_users[email]['socket'] = socket
-
-    def create_session(self, email):
-        if email:
-            if email in self.__logged_in_users and \
-                    'socket' in self.__logged_in_users[email]:
-                self.__logged_in_users[email]['socket'].close()
-
-            token = self.generate_token()
-            self.__logged_in_users[email] = {'token': token}
-            return token
-        return None
-
-    def end_session(self, token):
-        if self.has_valid_token(token):
-            email = self.get_email_by_token(token)
-            if (email != None):
-                del self.__logged_in_users[email]
-            return True
-        return False
 
 
 global session
-session = Session()
+session = new_session()
 
 
 def validate_signin(email, password):
--- src/webapp/static/client.js
+++ src/webapp/static/client.js
@@ -62,6 +62,39 @@ function renderPage() {
 
 /* ======= End ======= */
 
+
+/* ======= Crypto Helpers ======= */
+
+async function genKeys(){
+  if ('crypto' in window){
+    window.crypto.subtle.generateKey(
+      {
+          name: "RSA-OAEP",
+          modulusLength: 2048, //can be 1024, 2048, or 4096
+          publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+          hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+      },
+      false, //whether the key is extractable (i.e. can be used in exportKey)
+      ["encrypt", "decrypt"] //must be ["encrypt", "decrypt"] or ["wrapKey", "unwrapKey"]
+  )
+  .then(function(keyPair){
+      //returns a keypair object
+      console.log(keyPair);
+      console.log(keyPair.publicKey);
+      console.log(keyPair.privateKey);
+      window.sessionStorage.setItem('KEYS', keyPair)
+      
+    })
+  .catch(function(err){
+      console.error(err);
+  });
+  }
+}
+
+async function 
+
+/* ======= End ======= */
+
 /* ======= General Helpers ======= */
 
 function session(token) {
